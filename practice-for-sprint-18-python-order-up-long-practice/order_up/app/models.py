784b2641a4f21ce5e07c88f98bfed16f665df68e
George Merida <gmerida92@gmail.com>
2022-11-05 13:20:13 -0700

    Finished setting up database

--- practice-for-sprint-18-python-order-up-long-practice/order_up/app/models.py
+++ practice-for-sprint-18-python-order-up-long-practice/order_up/app/models.py
@@ -5,12 +5,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Employee(db.Model, UserMixin):
-    __tablename__ = 'employees'
+    __tablename__ = "employees"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement='auto')
     name = db.Column(db.String(100), nullable=False)
     employee_number = db.Column(db.Integer, nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+    orders = db.relationship("Order", back_populates="employee")
+
 
     @property
     def password(self):
@@ -22,3 +25,66 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Menu(db.Model):
+    __tablename__ = "menus"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    name = db.Column(db.String(30), nullable=False)
+
+    items = db.relationship("MenuItem", back_populates="menu")
+
+
+class MenuItem(db.Model):
+    __tablename__= "menu_items"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
+    menu_type_id = db.Column(db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False)
+
+    type = db.relationship("MenuItemType")
+    menu = db.relationship("Menu", back_populates="items")
+
+
+class MenuItemType(db.Model):
+    __tablename__ = "menu_item_types"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    name = db.Column(db.String(20), nullable=False)
+
+
+class Table(db.Model):
+    __tablename__= "tables"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    number= db.Column(db.Integer, nullable=False, unique=True)
+    capacity= db.Column(db.Integer, nullable=False)
+
+    orders = db.relationship("Order", back_populates="table")
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    employee_id = db.Column(db.Integer, db.foreignKey("employees.id"), nullable=False)
+    table_id = db.Column(db.Integer, db.foreignKey("tables.id"), nullable=False)
+    finished = db.Column(db.Boolean, nullable=False)
+
+    employee = db.relationship("Employee", back_populates="orders")
+    table = db.relationship("Table", back_populates="orders")
+    details = db.relationship("OrderDetail", back_populates="order")
+
+
+class OrderDetail(db.Model):
+    __tablename__ = "order_details"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+    order_id = db.Column(db.Integer, db.foreignKey("orders.id"), nullable=False)
+    menu_item_id = db.Column(db.Integer, db.foreignKey("menu_items.id"), nullable=False)
+
+    order = db.relationship("Order", back_populates="details")
+    menu_item = db.relationship("MenuItem")
--- practice-for-sprint-18-python-order-up-long-practice/order_up/database.py
+++ practice-for-sprint-18-python-order-up-long-practice/order_up/database.py
@@ -2,12 +2,49 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app import app, db
-from app.models import Employee
+from app.models import Employee, Menu, MenuItem, MenuItemType, Table
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
     employee = Employee(name="Margot", employee_number=1234, password="password")
+    
+    beverages = MenuItemType(name="Beverages")
+    entrees = MenuItemType(name="Entrees")
+    sides = MenuItemType(name="Sides")
+
+    dinner = Menu(name="Dinner")
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+    hangar = MenuItem(name="Hangar Steak", price=61.00, type=entrees, menu=dinner)
+
+    table1 = Table(number=1, capacity=4)
+    table2 = Table(number=2, capacity=2)
+    table3 = Table(number=3, capacity=4)
+    table4 = Table(number=4, capacity=4)
+    table5 = Table(number=5, capacity=4)
+    table6 = Table(number=6, capacity=2)
+    table7 = Table(number=7, capacity=4)
+    table8 = Table(number=8, capacity=2)
+    table9 = Table(number=9, capacity=2)
+    table10 = Table(number=10, capacity=6)
+
+
     db.session.add(employee)
+    db.session.add(dinner)
+
+    db.session.add(table1)
+    db.session.add(table2)
+    db.session.add(table3)
+    db.session.add(table4)
+    db.session.add(table5)
+    db.session.add(table6)
+    db.session.add(table7)
+    db.session.add(table8)
+    db.session.add(table9)
+    db.session.add(table10)
+
     db.session.commit()
